8caabd711cf252676a05f1b24c7dd2b0497a8a44
Denis Kulakov@ <denis.v.kulakov@gmail.com>
2018-05-10 12:25:24 +0300

    Удалили строку

--- week5.py
+++ week5.py
@@ -1,11 +1,10 @@
 def get_vat(payment, percent=18):
 	try:
 		payment = float(payment)
-		vat = payment / 100 * 18
+		percent = int(percent)
+		vat = payment / 100 * percent
 		vat = round(vat, 2)
 		return 'Сумма НДС: {}%'.format(vat)
 	except (TypeError, ValueError):
 		return 'Не могу посчитать, проверьте вводимые данные.'
-
-resutl = get_vat()
-print(resutl)
+		
